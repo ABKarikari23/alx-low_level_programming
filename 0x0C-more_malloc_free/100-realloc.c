0efbfa76d31c8ad7b86cfd3dd3e7ba93091b1eed
ABKarikari23 <abkarikari23@gmail.com>
2023-03-16 02:18:55 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
- * _realloc - Main Entry
- * @ptr: input
- * @old-size: input
- * @new_size: input
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previsouly allocated by malloc
+ * @old-size: size of the allocated memory for ptr
+ * @new_size: new size of the new memory block
  *
  * Return: 0
  */
