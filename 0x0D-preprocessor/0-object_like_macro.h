5fe8822c896bda88d315989966dd8867f4ec9f1d
ABKarikari23 <abkarikari23@gmail.com>
2023-03-20 06:54:30 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef object_like_macro_h_
-#define object_like_macro_h_
+#ifndef _object_like_macro_h_
+#define _object_like_macro_h_
 
 #define SIZE 1024
 
-#endif /* _object_like_macro_h_ */ 
+#endif
